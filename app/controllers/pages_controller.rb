b3a0b0e2e5e18342d639362eb81974d2b390ea30
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-04-29 10:42:02 +0100

    MyAIvendor3 day 1 - Transfering into lancelot2 file fourth attempt

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def faq
+  end
+
+  def how
+  end
+
+  def demo
+  end
+
+  def demo_request
+  end
+
+  def features
+  end
+end
